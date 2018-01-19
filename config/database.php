19588bf84ca8d3150272afbdcc4d9c88fa779d2c
Tong <15142016025@163.com>
2018-01-19 15:42:55 +0800

    alter  database
    alter database

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '182.92.235.154'),
+            'host' => env('DB_HOST', '47.93.127.4'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'fhqdb'),
             'username' => env('DB_USERNAME', 'fhq'),
